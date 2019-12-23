1091d08712e5922487a1dbf7fcb59b7ded715a4b
Albert Rayneer <rayneerqueiroz@gmail.com>
2019-12-23 18:04:51 -0300

    Time Change

--- testando_pysat.py
+++ testando_pysat.py
@@ -10,9 +10,6 @@ for clausula in clausulas:
     print(clausula)
     g.add_clause(clausula)
 
-start = time.time()
 print(g.solve())
-end = time.time()
-print(end - start)
 time = timeit.timeit('"-".join(str(n) for n in range(100))', number=1000)
 print("Executado em", round(time, 4), "milisegundos")
